748cb2388af0ca30ceca3dfd2495d7493e764aab
manavjeet singh <manavjeetsingh@manavjeets-MacBook-Air.local>
2025-01-01 12:00:56 +0530

    asyncHandler

--- src/index.js
+++ src/index.js
@@ -8,3 +8,16 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR:",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MOGNO db connection failed !!!", err);
+})
